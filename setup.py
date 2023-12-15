ddd74ee034125f5b4ee1c92672a7e2aa44e33dd6
ollyjohnson <ollyjohnson0131@gmail.com>
2023-12-15 09:47:46 +0000

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="battleships-game-pkg-oj263",
-    version="0.1.0",
+    version="0.1.1",
     author="OJ",
     author_email="oj263@exeter.ac.uk",
     description="A battleships game",
